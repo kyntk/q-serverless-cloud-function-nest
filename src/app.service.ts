0210e0b6d581bbb48e8ae038ac3817a9501b2d25
Kaneko Yuto <kaneko.yuto@a-tm.co.jp>
2023-12-22 09:31:18 +0900

    Change response content-type

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Serverless Cloud Function!';
+  getHello(): { message: string } {
+    return { message: 'Hello Serverless Cloud Function!' };
   }
 }
